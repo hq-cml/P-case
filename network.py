57c53f98dc3ecbdfbdadad57cd7c38e2623127df
hq <hq_cml@163.com>
2014-10-04 22:41:54 +0800

    Update network.py

--- network.py
+++ network.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+封装网络发送请求，采用select 多路I/O复用，长连接模式
+'''
+import socket
+import select
+import sys
+import time
+import struct
+
+import traceback
 import config
+
+class Network(object):
+    #类成员
+    select_timeout = 3             # select超时时间
+    sockfd         = 0             # 与后端连接的fd
+    desc_idx       = 0             # 后端配置索引
+
+    def __init__(self): 
+        self.select_timeout = 3
+        self.sockfd         = 0
+        self.desc_idx       = 0
+    
+    def __del__(self):
+        print "destruct\n"
